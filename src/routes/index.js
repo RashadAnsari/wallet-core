2d52e3d7822dcb99678cccac04fc04622c4a226e
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-07-09 15:22:36 +0200

    Move to src

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+  res.json({
+    message: 'Hello World!',
+  });
 });
 
 module.exports = router;

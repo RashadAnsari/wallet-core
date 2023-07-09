22d586f72e1336838fb37744c1650f5396d7348c
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-07-09 18:50:18 +0200

    Fix vscode settings

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.json({
-    message: 'Hello World!',
-  });
+  res.json({ message: 'Hello World!' });
 });
 
 module.exports = router;

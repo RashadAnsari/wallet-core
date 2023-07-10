15f2284045d469d19661789ced53fed330cea84c
Rashad Ansari <rashad.ansari1996@gmail.com>
2023-07-10 18:31:43 +0200

    Implement get transaction url endpoint

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const healthRouter = require('./routes/health');
 const hdwalletRouter = require('./routes/hdwallet');
+const transactionRouter = require('./routes/transaction');
 const { initWasm } = require('@trustwallet/wallet-core');
 
 const app = express();
 app.use(express.json());
 app.use('/', healthRouter);
 app.use('/', hdwalletRouter);
+app.use('/', transactionRouter);
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(err, req, res, next) {
---
+++ src/routes/transaction.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const joi = require('joi');
+
+const getTransactionUrlSchema = joi.object({
+  coin_type: joi.number().min(0).required(),
+  transaction_hash: joi.string().min(0).required(),
+});
+
+router.get('/v1/transaction/url', async function (req, res, next) {
+  const { error } = getTransactionUrlSchema.validate(req.query);
+  if (error) {
+    const errorMessages = error.details.map((detail) => detail.message);
+    return res.status(400).json({ errors: errorMessages });
+  }
+
+  const { CoinTypeConfiguration } = req.app.locals.core;
+  const { coin_type, transaction_hash } = req.query;
+
+  try {
+    const coinType = { value: coin_type };
+    const url = CoinTypeConfiguration.getTransactionURL(
+      coinType,
+      transaction_hash,
+    );
+
+    const response = { url: url };
+
+    res.json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
